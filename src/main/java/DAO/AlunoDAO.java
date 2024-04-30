c5aab01e9c2e736b1305c8bbf8fd35114aec1a4a
carlo <carlo@10.150.65.153>
2024-04-30 20:46:34 -0300

    Inicio

--- src/main/java/DAO/AlunoDAO.java
+++ src/main/java/DAO/AlunoDAO.java
@@ -1,7 +1,22 @@
 package DAO;
-
-
+import java.util.ArrayList;
+import model.Aluno;
 
 public class AlunoDAO {
-    
+    public static ArrayList<Aluno>minhaLista= new Arraylist<>();
+    public static ArrayList<Aluno>getMinhaLista(){
+   return minhaLista;   
+    }
+    public static void setMinhaLista(ArrayList<Aluno>minhaLista){
+        AlunoDAO.minhaLista= minhaLista;
+    }
+    public static int maiorID(){
+        int maiorId=0;
+        for(int i=0;i<minhaLista.size();i++){
+            if(minhaLista.get(i).getId()>maiorId){
+                maiorId=minhaLista.get(i).getId();
+            }
+        }  
+        return maiorId; 
+    }
 }

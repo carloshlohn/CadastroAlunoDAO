7ff7c581460fd4f8e1513c4f53e6195c24862843
carlo <carlo@10.150.65.153>
2024-04-30 20:41:22 -0300

    Inicio

--- src/main/java/model/Aluno.java
+++ src/main/java/model/Aluno.java
@@ -1,7 +1,42 @@
 package model;
 
+import javax.swing.JOptionPane;
+
 
 
 public class Aluno {
+    private String curso;
+    private int fase;
     
+    public Aluno() {
+        this("",0);
+    }
+
+    public Aluno(String curso, int fase) {
+        this.curso = curso;
+        this.fase = fase;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public int getFase() {
+        return fase;
+    }
+
+    public void setFase(int fase) {
+        this.fase = fase;
+    }
+    public void leitura() {
+        setCurso(JOptionPane.showInputDialog("Digite seu curso: "));
+        setFase(Integer.parseInt(JOptionPane.showInputDialog("Em qual fase você está: ")));
+    }
+    public void imprimir() {
+        JOptionPane.showMessageDialog(null,"Curso: " + getCurso() + "\nFase: " + getFase());
+    }
 }
--- src/main/java/model/Pessoa.java
+++ src/main/java/model/Pessoa.java
@@ -1,7 +1,50 @@
 package model;
 
-
+import javax.swing.JOptionPane;
 
 public class Pessoa {
-    
+    private int id;
+    private int idade;
+    private String nome;
+
+    public Pessoa() {
+        this(0,0,"");
+    }
+    public Pessoa(int id, int idade, String nome) {
+        this.id = id;
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void leitura() {
+        setId(Integer.parseInt(JOptionPane.showInputDialog("Digite seu ID:")));
+        setNome(JOptionPane.showInputDialog("Digite seu nome"));
+        setIdade(Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade:")));
+    }
+    public void imprimir() {
+        JOptionPane.showMessageDialog(null,"Nome: " + getNome() + "\nID: " + getId() + "\nIdade: " + getIdade());
+    }
 }
